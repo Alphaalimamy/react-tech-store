72db81d299dc954a853da14822c0ac66b090bd3e
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-06-12 02:19:31 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import Default from "./pages/Default";
+import SingleProductPages from "./pages/SingleProductPages";
+
+import { FaHome } from "react-icons/fa";
+
 function App() {
-  return <h1>Hello from tech store</h1>;
+  return (
+    <div className="">
+      <AboutPage />
+      <HomePage />
+      <ContactPage />
+      <ContactPage />
+
+      <Default />
+      <SingleProductPages />
+      <FaHome />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/SingleProductPages.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SingleProductPages() {
+  return (
+    <div>
+      <h1>Hello from SingleProductPages</h1>
+    </div>
+  );
+}
